9f1d9990b58c984f0bef13281041f8c453c8fcd2
Jenith <65620849+Jenith2k15@users.noreply.github.com>
2024-08-20 17:10:28 +0530

    gitignore

---
+++ MyApp/Controllers/PersonController.cs
@@ -0,0 +1,119 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MyApp.Data;
+
+namespace MyApp.Controllers
+{
+    public class PersonController : Controller
+    {
+        private readonly azure_app_trev_vs.Data.AppDbContext _context;
+
+        public PersonController(azure_app_trev_vs.Data.AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            if (_context.Persons != null)
+            {
+                List<Person> persons = await _context.Persons.ToListAsync();
+                return View(persons);
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateAsync(Person person)
+        {
+            if (!ModelState.IsValid || _context.Persons == null || person == null)
+            {
+                return View();
+            }
+            _context.Persons.Add(person);
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Persons == null)
+            {
+                return NotFound();
+            }
+
+            var person = await _context.Persons.FirstOrDefaultAsync(m => m.Id == id);
+            if (person == null)
+            {
+                return NotFound();
+            }
+            Person personResult = person;
+            return View(personResult);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(Person person)
+        {
+            if (!ModelState.IsValid || _context.Persons == null || person == null)
+            {
+                return View();
+            }
+            _context.Attach(person).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Persons == null)
+            {
+                return NotFound();
+            }
+
+            var person = await _context.Persons.FirstOrDefaultAsync(m => m.Id == id);
+            if (person == null)
+            {
+                return NotFound();
+            }
+            Person personResult = person;
+            return View(personResult);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(Person person)
+        {
+            if (!ModelState.IsValid || _context.Persons == null || person == null)
+            {
+                return View();
+            }
+            _context.Persons.Remove(person);
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Persons == null)
+            {
+                return NotFound();
+            }
+
+            var person = await _context.Persons.FirstOrDefaultAsync(m => m.Id == id);
+            if (person == null)
+            {
+                return NotFound();
+            }
+            Person personResult = person;
+            return View(personResult);
+        }
+    }
+}
---
+++ MyApp/Program.cs
@@ -0,0 +1,29 @@
+using azure_app_trev_vs.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+var connectionString = builder.Configuration.GetConnectionString("AppDbContext");
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(connectionString));
+
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+}
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
